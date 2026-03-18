64bb2fb0f66710dcb1de8060dac16cfa973d1585
keefer33 <me@jeffkeith.me>
2026-03-18 04:41:36 -0400

    add agents and composio

--- app/pages/generate/components/GenerateButton.tsx
+++ app/pages/generate/components/GenerateButton.tsx
@@ -1,13 +1,10 @@
-import { Box, Button, useMantineColorScheme } from "@mantine/core";
+import { Box, Button } from "@mantine/core";
 import { RiImageLine, RiVideoLine } from "@remixicon/react";
 import { TokensBadge } from "../../../shared/TokensBadge";
 import useGenerateStore from "~/lib/stores/generateStore";
-import useAppStore from "~/lib/stores/appStore";
 
 export function GenerateButton() {
-  const { isMobile } = useAppStore();
   const { getSelectedModel, generating, tokensCost } = useGenerateStore();
-  const { colorScheme } = useMantineColorScheme();
   const getIcon = () => {
     return getSelectedModel().generation_type === "video" ? (
       <RiVideoLine size={16} />
